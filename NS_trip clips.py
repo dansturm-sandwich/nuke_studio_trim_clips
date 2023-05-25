6102be19c02cf5f52800099ce957a4920069c779
dansturm-sandwich <dan@sandwich.co>
2023-05-25 10:48:44 -0700

    Initial commit

---
+++ NS_trip clips.py
@@ -0,0 +1,43 @@
+import nuke
+import nukescripts
+import hiero.core
+import hiero.ui
+from PySide2 import QtCore, QtWidgets
+from nukescripts import panels
+
+
+class TrimClipsPanel(nukescripts.PythonPanel):
+
+    def __init__(self):
+        nukescripts.PythonPanel.__init__(self, "Trim Clips")
+        self.trim_frames = nuke.Int_Knob('trim_frames', 'Trim Frames')
+        self.trim_frames.setValue(8)
+        self.addKnob(self.trim_frames)
+        self.trim_button = nuke.PyScript_Knob('trim_button', 'Trim Clips', command='')
+        self.addKnob(self.trim_button)
+
+    def knobChanged(self, knob):
+        if knob is self.trim_button:
+          self.trim_clips()
+
+    def trim_clips(self):
+        frames = int(self.trim_frames.getValue())
+        selection = hiero.ui.getTimelineEditor(hiero.ui.activeSequence()).selection()
+        for item in selection:
+            if isinstance(item, hiero.core.TrackItem):
+                in_offset = item.timelineIn()
+                out_offset = item.timelineOut() - (2 * frames)
+                srcin_offset = item.sourceIn() + frames
+                srcout_offset = item.sourceOut() - frames
+                item.setTimes(in_offset, out_offset, srcin_offset, srcout_offset)
+
+
+# panel = TrimClipsPanel()
+# panel.show()
+
+def climTripsPanel():
+    climTripsPanelInstance = TrimClipsPanel()
+    # climTripsPanelInstance.show()
+    nuke.TrimClipsPanel = panels.registerWidgetAsPanel('TrimClipsPanel()', 'Trim Clips', 'com.dansturm.TrimClipsPanel')
+
+climTripsPanel()
